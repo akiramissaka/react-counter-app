55de06183e45bb865ea9e79bef0f1cfefb5ea9e7
Akira Missaka <akira.missaka@corebiz.com.br>
2018-10-04 20:24:06 -0300

    final

---
+++ src/components/counters.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Counter from './counter.jsx';
+
+class Counters extends Component {
+	render() {
+
+		const { onReset, counters, onDelete, onIncrement } = this.props
+
+		return ( 
+			<div>
+				<button
+					onClick={onReset}
+					className="btn btn-primary btn-sm m-2"
+				>
+					Reset
+				</button>
+				{counters.map(counter => 
+					<Counter
+						key={ counter.id }
+						onDelete={ onDelete }
+						onIncrement={ onIncrement }
+						counter={ counter }
+					>
+						<h5>Counter #{ counter.id }</h5>
+					</Counter> 
+				)}
+			</div>
+		);
+	}
+}
+
+export default Counters;
